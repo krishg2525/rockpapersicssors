f6057a3476a5e837878e0b53e320db70f72b4772
Krish <krishgupta4592@gmail.com>
2024-07-07 15:25:26 +0530

    Initial commit

---
+++ game.js
@@ -0,0 +1,67 @@
+console.log("Hello World");
+let userScore = 0;
+let compScore = 0;
+
+const choices=document.querySelectorAll(".choice");
+const msg=document.querySelector("#msg");
+
+const userScorepara=document.querySelector("#user-score");
+const compScorepara=document.querySelector("#comp-score");
+
+const genCompChoice=()=>{
+    const options=["rock", "paper", "scissors"];
+    const randIdx=Math.floor(Math.random()*3);
+    return options[randIdx];
+
+}
+const drawGame =()=>{
+   msg.innerText="Game was draw.";
+
+}
+
+const showWinner=(userWin)=>{
+    if(userWin){
+        userScore++;
+        userScorepara.innerText=userScore;
+        msg.innerText="You win!";
+    }
+    else{
+        compScore++;
+        compScorepara.innerText=compScore;
+        msg.innerText="You loose!";
+    }
+}
+
+const playGame=(userChoice)=>{
+    console.log("user choice = ", userChoice);
+    // Genrate computer choice
+    const compchoice=genCompChoice();
+    console.log("comp choice =", compchoice);
+
+    if(userChoice===compchoice){
+        //Draw Game
+        drawGame();
+    } else{
+        let userWin =true;
+        if(userChoice==="rock"){
+            userWin=compchoice==="paper"?false:true;}
+            else if(userChoice==="paper"){
+                userWin=compchoice==="scissors"?false:true;
+             }
+             else{
+                userWin=compchoice==="rock"?false:true
+             }
+             showWinner(userWin);
+    }
+
+    }
+
+
+
+
+choices.forEach((choice)=>{
+    choice.addEventListener("click",()=>{
+        const userChoice=choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
